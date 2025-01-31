891a840ca57e3f4c832bc6235349dfcb1211d45f
sim <git@sgougeon.fr>
2025-01-31 08:52:27 +0000

    Use new callback after channel creation

--- app/src/main/java/org/unifiedpush/distributor/sunup/Distributor.kt
+++ app/src/main/java/org/unifiedpush/distributor/sunup/Distributor.kt
@@ -3,6 +3,7 @@ package org.unifiedpush.distributor.sunup
 import android.content.Context
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
+import org.unifiedpush.distributor.ChannelCreationStatus
 import org.unifiedpush.distributor.Database
 import org.unifiedpush.distributor.UnifiedPushDistributor
 import org.unifiedpush.distributor.sunup.api.MessageSender
@@ -20,11 +21,12 @@ object Distributor : UnifiedPushDistributor() {
     override fun registerChannelIdToServer(
         context: Context,
         packageName: String,
+        channelId: String?,
         title: String?,
         vapid: String?,
-        callback: (String?) -> Unit
+        callback: (ChannelCreationStatus) -> Unit
     ) {
-        val uuid = Uuid.random().toString()
+        val uuid = channelId ?: Uuid.random().toString()
         MessageSender.send(
             context,
             ClientMessage.Register(
@@ -32,8 +34,8 @@ object Distributor : UnifiedPushDistributor() {
                 key = vapid
             )
         )
-        // We call the callback even without the endpoint, it will send the REGISTER later
-        callback(uuid)
+        // REGISTER will be send later, when we received the new endpoint from autopush
+        callback(ChannelCreationStatus.Ok(channelId = uuid, sendEndpoint = false))
     }
 
     override fun unregisterChannelIdToServer(context: Context, appToken: String, callback: (Boolean) -> Unit) {
