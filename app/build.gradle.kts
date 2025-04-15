abc8d1feda456af26e19cffeb91c420629b34ec4
sim <git@sgougeon.fr>
2025-04-15 16:55:09 +0200

    1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "org.unifiedpush.distributor.sunup"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.4"
+        versionCode = 10
+        versionName = "1.1.0"
 
         // buildConfigField("String", "DEFAULT_API_URL", "\"http://10.0.2.2:8088\"")
         buildConfigField("String", "DEFAULT_API_URL", "\"https://push.services.mozilla.com\"")
