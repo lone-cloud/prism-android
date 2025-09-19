d702c11f5d4888c4e1a98e10eabce56a95d2cbb9
sim <git@sgougeon.fr>
2025-09-19 09:33:07 +0200

    Use new RegistrationState with ApplicationRowState

--- app/src/main/java/org/unifiedpush/distributor/sunup/activities/RegistrationsViewModel.kt
+++ app/src/main/java/org/unifiedpush/distributor/sunup/activities/RegistrationsViewModel.kt
@@ -5,9 +5,6 @@ import org.unifiedpush.android.distributor.ui.compose.state.RegistrationListStat
 import org.unifiedpush.android.distributor.ui.compose.state.RegistrationState
 import org.unifiedpush.distributor.Database
 import org.unifiedpush.distributor.sunup.DatabaseFactory
-import org.unifiedpush.distributor.utils.appInfoForMetadata
-import org.unifiedpush.distributor.utils.getApplicationIcon
-import org.unifiedpush.distributor.utils.getApplicationName
 
 fun getRegistrationListState(context: Context): RegistrationListState {
     return RegistrationListState(
@@ -26,19 +23,8 @@ fun getRegistrationListState(context: Context): RegistrationListState {
 
 fun getRegistrationState(context: Context, db: Database, token: String): RegistrationState? {
     val app = db.getAppFromCoToken(token) ?: return null
-    val ai = context.appInfoForMetadata(app.packageName)
-    val title = ai?.let { context.getApplicationName(it) } ?: app.packageName
-    val icon = context.getApplicationIcon(app.packageName)
-    val description = if (title == app.packageName) {
-        app.description ?: ""
-    } else {
-        app.description?.let { "$it — ${app.packageName}" }
-            ?: app.packageName
-    }
-    return RegistrationState(
-        icon = icon,
-        title = title,
-        description = description,
+   return RegistrationState(
+        app = context.applicationRowState(app.packageName, app.description),
         msgCount = app.msgCount,
         token = token,
         copyable = false
