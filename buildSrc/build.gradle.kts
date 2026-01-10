8918e0394c9dff17f33351dcfd729694ac1cf525
sim <git@sgougeon.fr>
2026-01-10 19:27:33 +0100

    Bump dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,9 +8,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:builder:8.13.0") // version Android Gradle Plugin
-    implementation("com.android.tools:common:31.2.2")
-    implementation("com.android.tools.build:bundletool:1.18.2")
+    implementation("com.android.tools.build:builder:8.13.2") // version Android Gradle Plugin
+    implementation("com.android.tools:common:31.13.2")
+    implementation("com.android.tools.build:bundletool:1.18.3")
     implementation("com.android.tools:sdklib:31.2.2")
-    implementation("com.android.tools.build:gradle-api:8.13.0")
+    implementation("com.android.tools.build:gradle-api:8.13.2")
 }
