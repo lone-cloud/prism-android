0d90e1dc21d8ddb7456e451d4f1e396482f00c3b
sim <git@sgougeon.fr>
2026-01-11 09:05:54 +0100

    Migrate from pass to passage

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -2,14 +2,14 @@
 
 [ $# -ne 1 ] || [ ! -f $1 ] && echo "Usage: $0 {version.aab}" && exit 1
 
-pass ls | grep sunup.key >/dev/null
+passage ls | grep sunup.key >/dev/null
 if [ $? -ne 0 ]; then
   echo "Pass keystore/sunup.key not found. Aborting."
   exit 1
 fi
 
-export KS="$HOME/.password-store/keystore/sunup.jks"
-export KS_PASS=$(pass keystore/sunup.key)
+export KS="$HOME/.passage/store/keystore/sunup.jks"
+export KS_PASS=$(passage keystore/sunup.key)
 export KEY_ALIAS="sunup"
 export AAB="$1"
 
