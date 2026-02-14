9bbe0dc67837e0e66f15b40a5123a90addc176f1
lone-cloud <lonecloud604@pm.me>
2026-02-14 01:09:16 -0800

    meeting apache v2 attribution requirements with code headers

--- app/src/main/java/app/lonecloud/prism/activities/MainActivity.kt
+++ app/src/main/java/app/lonecloud/prism/activities/MainActivity.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2024 p1gp1g
+ * Modified by lone-cloud under AGPL v3.0
+ *
+ * Original work: https://codeberg.org/Sunup/android
+ * Licensed under Apache License 2.0
+ */
+
 package app.lonecloud.prism.activities
 
 import android.os.Bundle
--- app/src/main/java/app/lonecloud/prism/services/RestartWorker.kt
+++ app/src/main/java/app/lonecloud/prism/services/RestartWorker.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2024 p1gp1g
+ * Modified by lone-cloud under AGPL v3.0
+ *
+ * Original work: https://codeberg.org/Sunup/android
+ * Licensed under Apache License 2.0
+ */
+
 @file:Suppress("ktlint:standard:no-wildcard-imports", "WildcardImport")
 
 package app.lonecloud.prism.services
